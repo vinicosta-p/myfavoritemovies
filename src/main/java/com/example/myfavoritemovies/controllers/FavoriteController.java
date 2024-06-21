e4ec9f7e777d819bd6392f5fb92cd9b827a638ea
Vinicius Costa <vini.p.f.costa@hotmail.com>
2024-06-21 17:06:45 -0300

    comunicação com api the movie database

---
+++ src/main/java/com/example/myfavoritemovies/controllers/FavoriteController.java
@@ -0,0 +1,9 @@
+package com.example.myfavoritemovies.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class FavoriteController {
+
+}
---
+++ src/test/java/com/example/myfavoritemovies/MyfavoritemoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfavoritemovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfavoritemoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
